9f9e088bea77762c9eace7249027652a54be3404
Beamlak Tesfahun <beamlakt24@gmail.com>
2024-11-24 19:45:21 +0300

    added entry endpoint for book

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { connectDB } from "./config/dbConnection.js";
+import bookRoute from "./routes/bookRoute.js";
 
 // load env variables
 dotenv.config();
@@ -19,6 +20,9 @@ app.use(cors());
 
 // for parsing json bodies
 app.use(express.json());
+
+// routes
+app.use("/books", bookRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
