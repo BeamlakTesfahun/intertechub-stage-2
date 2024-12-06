e3af57257b971375082180dd5e81d1da6c52f90b
Beamlak Tesfahun <beamlakt24@gmail.com>
2024-12-06 09:51:41 +0300

    added export to the function

--- middlewares/authorizeRole.js
+++ middlewares/authorizeRole.js
@@ -1,4 +1,4 @@
-const authorizeRole = (roles) => {
+export const authorizeRole = (roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
       return res.status(403).send("Access denied.");
